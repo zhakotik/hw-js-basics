617f2fde6bd93ca894135e7df90a84d5916256d2
Valentyna Zhakot <valushazhakot@gmail.com>
2023-08-30 20:04:44 +0300

    1st commit

--- homework3.js
+++ homework3.js
@@ -2,46 +2,46 @@
 1. Написати функцію, яка перетворює строку на масив слів,
     сортує його за алфавітним порядком і повертає (у масиві не повинно бути пробілів як елементів)
  */
-//  const str = "Це довільний текст для створення масиву слів ";
+ const str = "Це довільний текст для створення масиву слів ";
 
-//   function stringToArray(str) {
-//     const arr = str.toLowerCase().split(' ').sort();
-//     const result = arr.filter((element) => element !== '');
-//     return result;
-// }
+  function stringToArray(str) {
+    const arr = str.toLowerCase().split(' ').sort();
+    const result = arr.filter((element) => element !== '');
+    return result;
+}
 
 
-//   console.log(stringToArray(str));
+  console.log(stringToArray(str));
     
     
     // 2. Написати функцію, яка видаляє з масива елементи, що дублюються, та повертає масив оригінальних елементів,
     // відсортованих за зростанням
     
-    // const initialArray = [1, 4, 6, 6, 7, 5, 34, 5, 1000, 66, 1];
-    // function removeDuplicatesAndSort(arr) {
-    //     const sortedArray = arr.sort((a, b) => a - b);
-    //     return Array.from(new Set(sortedArray));
-    // }
+    const initialArray = [1, 4, 6, 6, 7, 5, 34, 5, 1000, 66, 1];
+    function removeDuplicatesAndSort(arr) {
+        const sortedArray = arr.sort((a, b) => a - b);
+        return Array.from(new Set(sortedArray));
+    }
     
-    // console.log(removeDuplicatesAndSort(initialArray)); 
+    console.log(removeDuplicatesAndSort(initialArray)); 
 
 
     // /*
     // 3. Написати функцію, яка поверне масив парних чисел
     //  */
 
-    // const initialArray2 = [2, 3, 6, 7, 9, 12];
-    // function arrayOfEvens(arr) {
-    //     const newArr = [];
-    //     for (element of arr) {
-    //       if (element % 2 === 0) {
-    //         newArr.push(element)
-    //       }
-    //     }
-    //     return newArr;
-    // }
+    const initialArray2 = [2, 3, 6, 7, 9, 12];
+    function arrayOfEvens(arr) {
+        const newArr = [];
+        for (element of arr) {
+          if (element % 2 === 0) {
+            newArr.push(element)
+          }
+        }
+        return newArr;
+    }
     
-    // console.log(arrayOfEvens(initialArray2)); 
+    console.log(arrayOfEvens(initialArray2)); 
     
     // /*
     // 4. Написати функцію, яка сформує масив з послідовності чисел, формуючи його з аргумента, який збільшує
